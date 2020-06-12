343cdab118957447f8b69e04a9201ab03339f173
chanyu <flashyunchen@gmail.com>
2020-06-12 21:19:57 +0800

    id改为自增

--- doc/sql/sys_user.sql
+++ doc/sql/sys_user.sql
@@ -1,42 +1,46 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : Localhost
-Source Server Version : 50719
-Source Host           : localhost:3306
-Source Database       : guns
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80020
+ Source Host           : localhost:3306
+ Source Schema         : flash
 
-Target Server Type    : MYSQL
-Target Server Version : 50719
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80020
+ File Encoding         : 65001
 
-Date: 2020-05-29 21:34:20
+ Date: 12/06/2020 21:17:46
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user` (
-  `id` bigint(20) NOT NULL COMMENT '主键id',
-  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
-  `account` varchar(45) DEFAULT NULL COMMENT '账号',
-  `password` varchar(45) DEFAULT NULL COMMENT '密码',
-  `salt` varchar(45) DEFAULT NULL COMMENT 'md5密码盐',
-  `name` varchar(45) DEFAULT NULL COMMENT '名字',
-  `birthday` datetime DEFAULT NULL COMMENT '生日',
-  `sex` varchar(32) DEFAULT NULL COMMENT '性别(字典)',
-  `email` varchar(45) DEFAULT NULL COMMENT '电子邮件',
-  `phone` varchar(45) DEFAULT NULL COMMENT '电话',
-  `role_id` varchar(255) DEFAULT NULL COMMENT '角色id(多个逗号隔开)',
-  `dept_id` bigint(20) DEFAULT NULL COMMENT '部门id(多个逗号隔开)',
-  `status` varchar(32) DEFAULT NULL COMMENT '状态(字典)',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `create_user` bigint(20) DEFAULT NULL COMMENT '创建人',
-  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  `update_user` bigint(20) DEFAULT NULL COMMENT '更新人',
-  `version` int(11) DEFAULT NULL COMMENT '乐观锁',
+CREATE TABLE `sys_user`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `account` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `salt` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'md5密码盐',
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名字',
+  `birthday` datetime(0) NULL DEFAULT NULL COMMENT '生日',
+  `sex` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别(字典)',
+  `email` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电子邮件',
+  `phone` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话',
+  `role_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色id(多个逗号隔开)',
+  `dept_id` bigint(0) NULL DEFAULT NULL COMMENT '部门id(多个逗号隔开)',
+  `status` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '状态(字典)',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `create_user` bigint(0) NULL DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `update_user` bigint(0) NULL DEFAULT NULL COMMENT '更新人',
+  `version` int(0) NULL DEFAULT NULL COMMENT '乐观锁',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='管理员表';
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '管理员表' ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
